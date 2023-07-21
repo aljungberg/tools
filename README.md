9a308f34cba89076c4364df974a82f1feea40bdc
Alexander Ljungberg <aljungberg@wireload.net>
2023-07-21 10:54:28 +0100

    Initial import.

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# tools
 Some of my personal tools and utilities.
+
+
+# Voice Memo Transcription Script
+
+This script is designed to automate the process of transcribing voice memos into text files. I've found that voice transcription technology has significantly improved in recent years, to the point where it has become a viable tool for daily journaling. With its efficiency and accuracy, it saves a considerable amount of time compared to traditional written notes, especially for those who may be prone to obsessing over the perfection of their written notes.
+
+My workflow with this tool is as follows:
+
+1.  The beauty of this process is in the 'record-and-forget' approach. I simply record my progress, thoughts and ideas, then move on with my work. This method bypasses the temptation to stop and perfect each entry as I would with written notes, eliminating a significant distraction from my workflow. I do not have to concern myself with what has been recorded until the next morning.
+2. Each voice memo is automatically synced to my computer (via iCloud in my case), where this Python script picks them up. It monitors my voice memo folder using a watchdog and transcribes each new file it detects into a text file.
+3. The script also measures the time between each note, which is very helpful when annotating the duration of tasks in my journal entries.
+4. The following morning, I review and lightly format the previous day's notes. This review process is streamlined by the high accuracy of the transcription and, as a result, usually requires only minor corrections.
+5. Naturally I pipeline the transcriptions through an LLM text model to do the initial formatting and to fix obvious transcription errors. This further minimizes the time I need to spend.
+
+This process has been an excellent substitute for traditional journaling for me. It allows a stream of consciousness style of recording my thoughts, tasks, and ideas, aiding in focusing my mind without getting caught up in the writing process itself. I also use journalling for my personal performance metrics which are roughly "how many useful things did I do today".
+
+This script uses OpenAI's Whisper model via [whisper.cpp](https://github.com/ggerganov/whisper.cpp) for transcription. 
+
+(Whisper is a little awkward to use real-time, so while initially I tried to just dictate live into my journal notes, that wasn't working well for me. The delay made it take longer than just writing, plus seeing the results on screen made me feel compelled to immediately edit and clean it up. With this script, I can just leave it running the background and when I compose my notes the next morning I basically just `cat` all the text files of yesterday in chronological order-- which is the same as alphabetical order given my naming scheme. Fire and forget.)
+
+I hope this script can be useful to others. Check the Python script for additional details and feel free to modify it to suit your specific needs.
+
+## Usage
+
+Follow the instructions to build [whisper.cpp](https://github.com/ggerganov/whisper.cpp). If you're on an Apple Silicon Mac, you might want to use the ANE version since it's much faster. (That said this script is meant to just run in the background so whether it takes 10 or 30 seconds per memo doesn't really matter that much.)
+
+```
+transcribe_voice_memos.py --whisper-path /my/path/whisper.cpp/main --model-file /my/path//whisper.cpp/models/ggml-medium.en.bin --memos-path ~/Library/Mobile\ Documents/com~apple~CloudDocs/Voice\ memos --watch
+```
+
+The `--watch` flag keeps it running the background so that when you need your transcriptions they're always ready. Transcribing a day's worth of notes isn't instant but if you don't mind the delay you can just run the tool as needed without `--watch`. It's idempotent.
---
+++ transcribe_voice_memos.py
@@ -0,0 +1,150 @@
+import argparse
+import os
+import re
+import subprocess
+import sys
+import time
+from datetime import datetime, timedelta
+
+from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
+
+# whisper.cpp timestamping annotation
+pattern = re.compile(r'\[(\d\d:\d\d:\d\d\.\d\d\d) --> (\d\d:\d\d:\d\d\.\d\d\d)\] (.*)')
+
+
+def transcribe_file(args, m4a_file, prev_end_time):
+    """Transcribe.
+
+    Note that we assume `prev_end_time` is always the end time of the penultimate transcription chronologically. So you need to call this method with the files sorted by date and time. (When using the watch feature, we implicitly assume that every new file that appears is newer than the previous one, which would be true if you're recording voice memos on your phone and syncing them to your computer like I do.)
+    """
+
+    m4a_path = os.path.join(args.memos_path, m4a_file)
+
+    transcript_file = os.path.splitext(m4a_file)[0] + '.txt'
+    transcript_path = os.path.join(args.memos_path, transcript_file)
+
+    # Check if transcription file exists and it is not older than the M4A file. This makes the script idempotent.
+    if os.path.exists(transcript_path) and os.path.getmtime(transcript_path) > os.path.getmtime(m4a_path):
+        # Make sure to update the prev_end_time as if we had transcribed the file.
+        with open(transcript_path, 'r') as tf:
+            lines = tf.readlines()
+            last_line = lines[-1].strip()
+            if last_line.endswith('.'):
+                last_line = last_line[:-1]
+            _, time_string = last_line.split("Memo ended at ")
+            return datetime.strptime(time_string, "%Y-%m-%d %H:%M:%S")
+
+    # Parse date and time from the file name
+    date_string = m4a_file.split("Recording ")[1].split(" at ")[0]
+    time_string = m4a_file.split(" at ")[1].split(".m4a")[0]
+    datetime_string = f"{date_string} {time_string}"
+    recording_datetime = datetime.strptime(datetime_string, "%Y-%m-%d %H.%M.%S")
+
+    # Convert M4A to temporary WAV file because whisper.cpp only supports wav. My files are named "Audio Recording 2023-07-17 at 13.16.43.m4a". So change this as needed for your own purposes, or use ctime.
+    temp_wav_path = os.path.join(args.memos_path, "temp.wav")
+    subprocess.run(["ffmpeg", "-y", "-i", m4a_path, "-ar", "16000", temp_wav_path], stderr=subprocess.DEVNULL)
+
+    # Transcribe the temporary WAV file using whisper.cpp. Tip: if on a modern Mac, compile the version that runs on the Apple Neural Engine, it's much faster. If you've done it right the ANE model is used automatically.
+    transcription_command = [
+        args.whisper_path,
+        "--model",
+        args.model_file,
+        temp_wav_path,
+    ]
+    if (result := subprocess.run(transcription_command, capture_output=True, text=True)).returncode != 0:
+        raise Exception("Transcription error: " + result.stderr)
+
+    # Write to the transcription text file
+    with open(transcript_path, 'w') as tf:
+        def write(_line):
+            """Output to both stdout and the transcription file."""
+            print(_line)
+            tf.write(_line + '\n')
+
+        # Every time a new day starts, print a new header. I use this when converting to journal entries.
+        if prev_end_time is None or prev_end_time.date() != recording_datetime.date():
+            write(f"First memo of day at {recording_datetime.strftime('%Y-%m-%d %H:%M:%S')}:")
+            prev_end_time = recording_datetime
+        else:
+            write(f"New memo at {recording_datetime.strftime('%Y-%m-%d %H:%M:%S')}:")
+
+        for line in result.stdout.splitlines():
+            if match := pattern.match(line):
+                start_time_str, end_time_str, transcript = match.groups()
+                start_offset = datetime.strptime(start_time_str, "%H:%M:%S.%f").time()
+                end_offset = datetime.strptime(end_time_str, "%H:%M:%S.%f").time()
+
+                start_time = recording_datetime + timedelta(hours=start_offset.hour, minutes=start_offset.minute, seconds=start_offset.second, microseconds=start_offset.microsecond)
+                end_time = recording_datetime + timedelta(hours=end_offset.hour, minutes=end_offset.minute, seconds=end_offset.second, microseconds=end_offset.microsecond)
+
+                time_since_prev = start_time - prev_end_time
+                if time_since_prev >= timedelta(minutes=1):
+                    minutes = time_since_prev.seconds // 60
+                    # These "x minutes later" lines are useful when converting to journal entries, giving me a quick gauge for how long I spent doing something between two voice memos. Since I push the text through a generative AI for first pass formatting, this really helps there too (current AI not being great at math, they're helped by having an explicit "x minutes later" line rather than timestamps).
+                    write(f"[ {minutes} minutes later... ]")
+
+                adjusted_line = f"> {transcript.strip()}"
+                write(adjusted_line)
+                prev_end_time = end_time
+            else:
+                # Ignore blank lines but fail on other unexpected lines
+                if line.strip():
+                    raise ValueError(f"Unexpected line: {line}")
+
+        write(f"Memo ended at {prev_end_time.strftime('%Y-%m-%d %H:%M:%S')}.")
+
+    os.remove(temp_wav_path)
+    return prev_end_time
+
+
+class M4AEventHandler(FileSystemEventHandler):
+    def __init__(self, fn):
+        super().__init__()
+        self.fn = fn
+
+    def on_modified(self, event):
+        if event.src_path.endswith(".m4a"):
+            self.fn(event.src_path)
+
+    def on_created(self, event):
+        if event.src_path.endswith(".m4a"):
+            self.fn(event.src_path)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--whisper-path', type=str, required=True, help='Path to the whisper.cpp binary')
+    parser.add_argument('--model-file', type=str, required=True, help='Path to the whisper model file')
+    parser.add_argument('--memos-path', type=str, required=True, help='Path to the directory containing the voice memos')
+    parser.add_argument('--watch', action='store_true', help='Keep the script running and watch for new or changed M4A files')
+    args = parser.parse_args()
+
+    last_end_time = None
+
+
+    def transcribe_one(m4a_file):
+        global last_end_time
+        sys.stderr.write(f"Transcribing {m4a_file}...\n")
+        last_end_time = transcribe_file(args, m4a_file, last_end_time)
+
+
+    # Get a sorted list of M4A files in the directory
+    m4a_files = sorted([f for f in os.listdir(args.memos_path) if f.endswith(".m4a")])
+    sys.stderr.write(f"Found {len(m4a_files)} recordings.\n")
+    for fname in m4a_files:
+        transcribe_one(fname)
+
+    if args.watch:
+        # Make sure not to start the watcher until after the initial transcription is done. We need `prev_end_time` to be up to date.
+        event_handler = M4AEventHandler(transcribe_one)
+        observer = Observer()
+        observer.schedule(event_handler, args.memos_path, recursive=False)
+        observer.start()
+
+        try:
+            while True:
+                time.sleep(5)  # keep the script running
+        except KeyboardInterrupt:
+            observer.stop()
+        observer.join()
